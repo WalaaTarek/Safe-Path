e6fc9b820cfe95b18e9c600189ad0249c2bc212e
xxesraahassan <xxesraahassan@gmail.com>
2026-04-13 18:49:21 +0200

    remove unwanted folders

--- blindassist_flutter/android/app/src/main/kotlin/com/example/blindassist_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.blindassist_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- blindassist_flutter/test/widget_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
- import 'package:flutter_test/flutter_test.dart';
- import 'package:blindassist_flutter/main.dart'; 
- void main() { testWidgets('Basic BlindAssistApp test', (WidgetTester tester) async { 
-  await tester.pumpWidget(BlindAssistApp());
-  expect(find.text('No objects detected'), findsOneWidget); });
-  }
