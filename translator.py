8fb94fa189084dcbe1569742caa0e5e5611b06df
SalmaHossam630 <salmahossam630@gmail.com>
2026-04-25 03:03:00 +0300

    Clean repository structure and keep only main project folders

--- translator.py
+++
@@ -1,8 +0,0 @@
-from deep_translator import GoogleTranslator
-
-def translate_text(text, target="ar"):
-    try:
-        translated = GoogleTranslator(source='auto', target=target).translate(text)
-        return translated
-    except Exception as e:
-        return f"Translation Error: {str(e)}"
